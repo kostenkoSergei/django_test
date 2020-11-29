7210e5e72494aaeb554562300cbbd88a9885d707
sergeiKostenko <sdh1985@mail.ru>
2020-11-29 16:50:43 +0300

    initial commit

---
+++ mysite/news/templatetags/news_tags.py
@@ -0,0 +1,17 @@
+from django import template
+from news.models import Category
+from django.db.models import Count
+
+register = template.Library()
+
+
+@register.simple_tag(name='get_list_categories')
+def get_categories():
+    return Category.objects.all()
+
+
+@register.inclusion_tag('news/list_categories.html')
+def show_categories(arg1='hello', arg2='world'):
+    # categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
+    return {'categories': categories, 'arg1': arg1, 'arg2': arg2}
